939ee713af382ed56b8a03a8ab8f0c09b01244b3
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-21 01:27:30 +0800

    add toggle color theme component , and config tailwindcss dark mode to <body>

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,34 @@ import "./App.css";
 import Home from "@routes/home";
 import Works from "@routes/works";
 import Projects from "@routes/projects";
+import ThemeToggle from "@components/ThemeToggle";
 
 function App() {
   return (
     <>
-      <div className="sticky top-0 mt-2 mb-10 p-2 bg-stone-600">
-        <div className="flex flex-row justify-center">
-          <nav>
-            <Link to="/"> HOME </Link>
-            <Link to="/works"> WORKS </Link>
-            <Link to="/projects"> PROJECTS </Link>
-          </nav>
-        </div>
-      </div>
-      <div className="card">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="works" element={<Works />} />
-          <Route path="projects" element={<Projects />} />
-        </Routes>
-      </div>
+      <html>
+        <body className="min-h-screen bg-stone-300 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-50 font-vr flex-col text-center pt-20 pb-5 duration-500">
+          <div className="sticky top-0 mt-2 mb-10 p-2 bg-stone-600">
+            <div className="flex flex-row justify-center items-center">
+              <nav>
+                <Link to="/"> HOME </Link>
+                <Link to="/works"> WORKS </Link>
+                <Link to="/projects"> PROJECTS </Link>
+              </nav>
+              <div className="place-content-end">
+                <ThemeToggle />
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="works" element={<Works />} />
+              <Route path="projects" element={<Projects />} />
+            </Routes>
+          </div>
+        </body>
+      </html>
     </>
   );
 }
