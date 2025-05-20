882f9c42c0bf2ca133865a39150fef10c6ad969f
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-21 03:05:07 +0800

    add variants for animation config

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -1,21 +1,26 @@
 import { motion } from "motion/react";
 import React from "react";
 
-const MotionDiv = ({
-  children,
-  delay,
-}: {
-  children: React.ReactNode;
-  delay: number;
-}) => (
-  <motion.div
-    className="flex flex-wrap justify-evenly gap-8"
-    initial={{ y: 30, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay: delay, ease: "linear" }}
-  >
-    {children}
-  </motion.div>
-);
+const MotionDiv = ({ children }: { children: React.ReactNode }) => {
+  const animateConfig = {
+    hidden: { y: 30, x: 0, opacity: 0 },
+    enter: { y: 0, x: 0, opacity: 1 },
+    exit: { y: 30, x: -0, opacity: 0 },
+  };
+
+  return (
+    <motion.article
+      className="flex flex-wrap justify-evenly gap-8"
+      variants={animateConfig}
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      transition={{ duration: 0.4, delay: 0.2, ease: "easeInOut" }}
+      style={{ position: "relative" }}
+    >
+      {children}
+    </motion.article>
+  );
+};
 
 export default MotionDiv;
