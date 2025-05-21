3957f516e0d0689b2e20e1696657594b5f2b3dd5
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-22 03:27:14 +0800

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router";
 import ThemeToggle from "@components/ThemeToggle";
+import { Box } from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
     <>
-      <div className="sticky top-0 mt-2 mb-10 p-2 bg-stone-600">
-        <div className="flex flex-row justify-center items-center">
-          <nav>
-            <Link to="/"> HOME </Link>
-            <Link to="/works"> WORKS </Link>
-            <Link to="/projects"> PROJECTS </Link>
-          </nav>
-          <div className="place-content-end">
-            <ThemeToggle />
-          </div>
-        </div>
-      </div>
+      <Box
+        width="100%"
+        padding="2"
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        gapX="5"
+      >
+        <nav>
+          <Link to="/"> HOME </Link>
+          <Link to="/works"> WORKS </Link>
+          <Link to="/projects"> PROJECTS </Link>
+        </nav>
+        <ThemeToggle />
+      </Box>
     </>
   );
 }
