fbfe423046db45f3ebb905ca31fdcc26112808b3
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-22 03:34:36 +0800

    change navbar chakraUI component layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,29 @@
 import { Link } from "react-router";
 import ThemeToggle from "@components/ThemeToggle";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
     <>
-      <Box
-        width="100%"
-        padding="2"
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        gapX="5"
-      >
-        <nav>
-          <Link to="/"> HOME </Link>
-          <Link to="/works"> WORKS </Link>
-          <Link to="/projects"> PROJECTS </Link>
-        </nav>
-        <ThemeToggle />
-      </Box>
+      <Flex direction="row" justifyContent="center">
+        <Box
+          width="80%"
+          paddingY={3}
+          paddingX={5}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          gapX="5"
+        >
+          <Flex direction="row" gapX={10}>
+            <Link to="/"> HOME </Link>
+            <Link to="/works"> WORKS </Link>
+            <Link to="/projects"> PROJECTS </Link>
+          </Flex>
+          <ThemeToggle />
+        </Box>
+      </Flex>
     </>
   );
 }
