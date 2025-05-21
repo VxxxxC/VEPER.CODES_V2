527080936a2b9071151e11f456d840b556fc6111
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-22 03:25:45 +0800

    add props to header

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -1,7 +1,14 @@
 import { motion } from "motion/react";
 import React from "react";
+import Head from "../Head";
+import type { HeaderType } from "../Head";
 
-const MotionDiv = ({ children }: { children: React.ReactNode }) => {
+export interface MotionDivType extends HeaderType {
+  children: React.ReactNode;
+}
+
+const MotionDiv = ({ title, description, children }: MotionDivType) => {
+  const tag = `${title} - VEPER.CODES`;
   const animateConfig = {
     hidden: { y: 30, x: 0, opacity: 0 },
     enter: { y: 0, x: 0, opacity: 1 },
@@ -18,6 +25,7 @@ const MotionDiv = ({ children }: { children: React.ReactNode }) => {
       transition={{ duration: 0.4, delay: 0.2, ease: "easeInOut" }}
       style={{ position: "relative" }}
     >
+      {title && <Head title={tag} description={description} />}
       {children}
     </motion.article>
   );
