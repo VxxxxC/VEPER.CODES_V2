2d9ec6679ee362e7b364b9a1235f6fe0974bdcd3
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-22 03:26:55 +0800

    change tailwindcss class to ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
-import Head from "./Head.tsx";
 import Navbar from "@components/NavBar.tsx";
 import Home from "@routes/home";
 import Works from "@routes/works";
 import Projects from "@routes/projects";
+import { Box, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra/components/ui/color-mode";
 
 function App() {
   return (
     <>
-      <html>
-        <Head title="VEPER.CODES" />
-        <body className="min-h-screen bg-stone-300 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-50 flex-col text-center pt-20 pb-5 transition duration-500 ease-in-out">
-          <Navbar />
-          <div className="card">
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="works" element={<Works />} />
-              <Route path="projects" element={<Projects />} />
-            </Routes>
-          </div>
-        </body>
-      </html>
+      <Box
+        height="100%"
+        width="100%"
+        bg={useColorModeValue("gray.300", "gray.850")}
+        transition="background 0.8s ease-in-out"
+        className="bg-stone-300 dark:bg-zinc-900 text-zinc-700 dark:text-zinc-50"
+      >
+        <SimpleGrid columns={5}>
+          <GridItem colSpan={3} colStart={2}>
+            <Navbar />
+            <div className="card">
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="works" element={<Works />} />
+                <Route path="projects" element={<Projects />} />
+              </Routes>
+            </div>
+          </GridItem>
+        </SimpleGrid>
+      </Box>
     </>
   );
 }
