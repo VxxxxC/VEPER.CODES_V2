a9f6ba6e0be965a9f3af6782bce654dfc4ed16ea
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-22 03:26:14 +0800

    edit header component

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,16 +1,13 @@
-export default function Head({
-  title,
-  description,
-}: {
-  title: string;
+export interface HeaderType {
+  title?: string;
   description?: string;
-}) {
+}
+
+export default function Head({ title, description }: HeaderType) {
   return (
     <>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
       <meta name="title" content={title} />
       <meta name="description" content={description} />
     </>
