9f7cad0b5a34e771d4d84413d90d47588b4b466d
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-23 20:38:34 +0800

    reused code , create a component to reuse

---
+++ src/components/NestedTab.tsx
@@ -0,0 +1,85 @@
+import { Box, Flex, Tabs } from "@chakra-ui/react";
+import {
+  useColorMode,
+  useColorModeValue,
+} from "@chakra/components/ui/color-mode";
+import { useEffect, useState } from "react";
+import MotionDiv from "./MotionDiv";
+
+export type tabsPropsType = {
+  id: string;
+  label: string;
+  content: React.ReactNode;
+};
+
+const NestedTab = ({ tabs }: { tabs: tabsPropsType[] }) => {
+  const [selectedTab, setSelectedTab] = useState<tabsPropsType["id"]>(
+    tabs[0].id,
+  );
+
+  const [textColor, setTextColor] = useState<string>("");
+  const [bgColor, setBgColor] = useState<string>("");
+
+  const { colorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode == "light") {
+      setTextColor("pink.400");
+      setBgColor("gray.900");
+    } else {
+      setTextColor("cyan.400");
+      setBgColor("gray.500");
+    }
+  }, [colorMode]);
+
+  return (
+    <>
+      <MotionDiv>
+        <Tabs.Root
+          value={selectedTab}
+          variant="plain"
+          size="sm"
+          orientation="vertical"
+          onValueChange={(e) => setSelectedTab(e.value)}
+        >
+          <Flex
+            justify="start"
+            bg={useColorModeValue("gray.100", "gray.800")}
+            rounded="lg"
+          >
+            {tabs.map(({ id, label }: tabsPropsType) => (
+              <Tabs.List rounded="md" padding={1}>
+                <Tabs.Trigger
+                  key={id}
+                  value={id}
+                  color={textColor}
+                  bg={selectedTab === id ? bgColor : ""}
+                >
+                  {label}
+                </Tabs.Trigger>
+              </Tabs.List>
+            ))}
+          </Flex>
+        </Tabs.Root>
+
+        <Tabs.Root
+          value={selectedTab}
+          variant="plain"
+          size="sm"
+          orientation="horizontal"
+          onValueChange={(e) => setSelectedTab(e.value)}
+        >
+          {tabs.map(({ id, content }: tabsPropsType) => (
+            <div>
+              <Box>
+                <Tabs.Content value={id}>{content}</Tabs.Content>
+              </Box>
+            </div>
+          ))}
+        </Tabs.Root>
+      </MotionDiv>
+    </>
+  );
+};
+
+export default NestedTab;
--- src/routes/works.tsx
+++ src/routes/works.tsx
@@ -1,13 +1,7 @@
-import MotionDiv from "@components/MotionDiv";
 import Boxs from "./boxs/boxs.tsx";
-import { Container, Flex, Tabs } from "@chakra-ui/react";
+import type { tabsPropsType } from "@components/NestedTab";
 import { v4 as uuidv4 } from "uuid";
-
-export type tabsPropsType = {
-  id: string;
-  label: string;
-  content: React.ReactNode;
-};
+import NestedTab from "@components/NestedTab";
 
 export default function Works() {
   const works: tabsPropsType[] = [
@@ -15,29 +9,7 @@ export default function Works() {
   ];
   return (
     <>
-      {
-        <div className="flex flex-col items-start justify-center w-full h-full">
-          {works.map(({ id, label, content }: tabsPropsType) => (
-            <>
-              <Tabs.Root defaultValue={works[0].id} variant="plain">
-                <Tabs.List aria-label="works tab">
-                  <Tabs.Trigger value={id} key={id} title={label}>
-                    {label}
-                  </Tabs.Trigger>
-                  <MotionDiv>
-                    <Container>
-                      <Flex>
-                        <Tabs.Content value={id}>{content}</Tabs.Content>
-                      </Flex>
-                    </Container>
-                  </MotionDiv>
-                  <Tabs.Indicator rounded={12} />
-                </Tabs.List>
-              </Tabs.Root>
-            </>
-          ))}
-        </div>
-      }
+      <NestedTab tabs={works} />
     </>
   );
 }
