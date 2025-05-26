38dc1cf11f50e7357fd0e1180837444bfa256ee0
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-26 19:25:25 +0800

    add animated typewriter at home

---
+++ src/components/HomeWelcomeMsg.tsx
@@ -0,0 +1,33 @@
+import { Box, Center } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
+import { useColorMode } from "@chakra/components/ui/color-mode";
+
+const text1 = "Hi there!! Thank you to visit VEPER.CODES";
+const text2 = "I am a Developer base in Hong Kong";
+const text3 = "To more about me, please have a look at the below :)";
+
+const HomeWelcomeMsg = () => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      bgColor={colorMode === "light" ? "gray.200" : "gray.800"}
+      rounded="md"
+      p={2}
+      m={5}
+    >
+      <Center>
+        <Typewriter
+          options={{
+            cursor: "",
+            strings: [text1, text2, text3],
+            delay: 60,
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Center>
+    </Box>
+  );
+};
+
+export default HomeWelcomeMsg;
