a13a17413a0cdd7b68acfa10cca3bb8d8ca8f599
vxxxxc <vxxxxc@Serpents-M3.local>
2025-05-31 03:41:55 +0800

    define custom provider config, define custom text style

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,43 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { ColorModeProvider } from "./src/components/ui/color-mode.tsx";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+  defaultSystem,
+  defineTextStyles,
+} from "@chakra-ui/react";
+
+const textStyles = defineTextStyles({
+  header: {
+    value: {
+      fontFamily: "Varela Round",
+      fontWeight: "800",
+      fontSize: "20px",
+    },
+  },
+  body: {
+    value: {
+      fontFamily: "Roboto",
+      fontWeight: "400",
+      fontSize: "16px",
+    },
+  },
+});
+
+const config = defineConfig({
+  theme: {
+    textStyles,
+  },
+});
+
+const system = createSystem(defaultConfig, config);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider>
         <BrowserRouter>
           <App />
