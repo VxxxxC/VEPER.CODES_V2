7a7e616f3338e80502b87116b8b3a5182a074242
vxxxxc <vxxxxc@Serpents-M3.local>
2025-06-11 03:55:37 +0800

    remove error boundary

--- src/router.tsx
+++ src/router.tsx
@@ -3,28 +3,23 @@ import Home from "@routes/home";
 import Works from "@routes/works";
 import Projects from "@routes/projects";
 import App from "./App";
-import ErrorBoundary from "@components/ErrorBoundary";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorBoundary />,
     children: [
       {
         index: true,
         element: <Home />,
-        errorElement: <ErrorBoundary />,
       },
       {
         path: "works",
         element: <Works />,
-        errorElement: <ErrorBoundary />,
       },
       {
         path: "projects",
         element: <Projects />,
-        errorElement: <ErrorBoundary />,
       },
     ],
   },
